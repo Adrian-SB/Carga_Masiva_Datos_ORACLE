a35539af9169daa06d1953d0f66a43c838506454
Adrián Silva <asboceta@gmail.com>
2019-02-26 14:53:36 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -9,17 +9,19 @@ read user
 echo 'Contraseña: '
 read pwd
 echo
-importar(){
+#####Funcion importar
+function importar(){
 echo 'Introduca el nombre de la tabla a la que desea importar los datos: \n'
 read tabla
 echo 
 sqlldr $user/$pwd control='Cargas/$tabla.ctl' data='Datos/$tabla.txt' log='/tmp/bd.log'
 echo '\nImportación finalizada\n'
 }
-#echo "¿Desea importar más datos?"
-#read result
-#	if [ $result == 'si' ]; then
-#		importar
-#	else
-#		echo 'Recuerde ojear el fichero de log que se encuentra en la ruta /tmp/bd.log' 
-#	fi
+###
+echo "¿Desea importar más datos?"
+read result
+	if [ $result == 'si' ]; then
+		importar
+	else
+	echo 'Recuerde ojear el fichero de log que se encuentra en la ruta /tmp/bd.log' 
+	fi
