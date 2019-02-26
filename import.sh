07b32a4e31fd947f65006898fd86be823f7d7a9f
Adrián Silva <asboceta@gmail.com>
2019-02-26 14:08:01 +0100

    ejecucion
    fuera \n e intento 1

--- import.sh
+++ import.sh
@@ -2,19 +2,20 @@
 
 echo 'CARGA DE DATOS MASIVA EN ORACLE'
 echo '-------------------------------'
-echo 'Por favor, introduzca el nombre de usuario de Oracle y su contraseña(adri/adri): \n'
+echo 'Por favor, introduzca el nombre de usuario de Oracle y su contraseña(adri/adri): '
+echo
 echo 'Usuario: '
 read user
 echo 'Contraseña: '
 read pwd
 echo
-	importar(){
-	echo 'Introduca el nombre de la tabla a la que desea importar los datos: \n'
-	read tabla
-	echo 
-	sqlldr $user/$pwd control='Cargas/$tabla.ctl' data='Datos/$tabla.txt' log='/tmp/bd.log'
-	echo '\nImportación finalizada\n'
-	}
+importar(){
+echo 'Introduca el nombre de la tabla a la que desea importar los datos: \n'
+read tabla
+echo 
+sqlldr $user/$pwd control='Cargas/$tabla.ctl' data='Datos/$tabla.txt' log='/tmp/bd.log'
+echo '\nImportación finalizada\n'
+}
 echo "¿Desea importar más datos?"
 read result
 	if [ $result == 'si' ]; then
